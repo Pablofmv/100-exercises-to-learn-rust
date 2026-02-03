dc29e38c439ec4ea75201c4c832b71392b30b98b
Pablo Mendoza <pablo_fmv@hotmail.com>
2026-02-03 11:23:51 -0500

    Completed 4.12 Copy Trait

--- exercises/04_traits/12_copy/src/lib.rs
+++ exercises/04_traits/12_copy/src/lib.rs
@@ -1,6 +1,9 @@
 // TODO: implement the necessary traits to make the test compile and pass.
 //  You *can't* modify the test.
 
+use std::ops::Add;
+
+#[derive(Copy,Clone, Debug, PartialEq)]
 pub struct WrappingU32 {
     value: u32,
 }
@@ -9,6 +12,17 @@ impl WrappingU32 {
     pub fn new(value: u32) -> Self {
         Self { value }
     }
+}
+
+impl Add for WrappingU32 {
+    type Output = WrappingU32;
+
+    fn add(self, rhs: WrappingU32) -> WrappingU32 {
+        WrappingU32 {
+            value: self.value.wrapping_add(rhs.value)
+        }
+    }
+
 }
 
 #[cfg(test)]
