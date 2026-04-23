7c2eb303a6217fb0b7444d24eb6627e9c5bf13e5
Pablo Mendoza <pablo_fmv@hotmail.com>
2026-04-23 12:18:39 -0400

    Completed 7.3

--- exercises/07_threads/03_leak/src/lib.rs
+++ exercises/07_threads/03_leak/src/lib.rs
@@ -6,7 +6,19 @@
 use std::thread;
 
 pub fn sum(v: Vec<i32>) -> i32 {
-    todo!()
+    
+    let v = v.leak();
+
+    let m = v.len() / 2;
+
+    let left = &v[..m];
+    let right = &v[m..];
+
+    let h1 = thread::spawn(move || left.iter().sum::<i32>());
+    let h2 = thread::spawn(move || right.iter().sum::<i32>());
+
+    h1.join().unwrap() + h2.join().unwrap()
+
 }
 
 #[cfg(test)]
